42c7450f502c3b96c9449096c941765bce602ce3
Justin Lee <JustinDellAdam@live.com>
2026-03-14 21:12:51 +0800

    Update MinSTLExporter.js

--- src/components/webgl/MinSTLExporter.js
+++ src/components/webgl/MinSTLExporter.js
@@ -1,117 +1,116 @@
 import * as THREE from 'three';
 
+const EXPORT_SCALE = 35;
+
+const vertexA = new THREE.Vector3();
+const vertexB = new THREE.Vector3();
+const vertexC = new THREE.Vector3();
+const cb = new THREE.Vector3();
+const ab = new THREE.Vector3();
+const normal = new THREE.Vector3();
+
+function getAvatarName() {
+    const rawAvatarName = typeof window !== 'undefined' && typeof window.avatarName === 'string'
+        ? window.avatarName
+        : '';
+
+    return rawAvatarName.trim().toLowerCase().replace(/\s+/g, '-') || 'exported';
+}
+
+function writeVertex(output, vertex) {
+    output.push(
+        '\t\t\tvertex ',
+        vertex.x * EXPORT_SCALE,
+        ' ',
+        vertex.y * EXPORT_SCALE,
+        ' ',
+        vertex.z * EXPORT_SCALE,
+        '\n'
+    );
+}
+
+function writeFace(output, mesh, positionAttribute, a, b, c) {
+    vertexA.fromBufferAttribute(positionAttribute, a);
+    vertexB.fromBufferAttribute(positionAttribute, b);
+    vertexC.fromBufferAttribute(positionAttribute, c);
+
+    if (mesh.isSkinnedMesh === true) {
+        mesh.applyBoneTransform(a, vertexA);
+        mesh.applyBoneTransform(b, vertexB);
+        mesh.applyBoneTransform(c, vertexC);
+    }
+
+    vertexA.applyMatrix4(mesh.matrixWorld);
+    vertexB.applyMatrix4(mesh.matrixWorld);
+    vertexC.applyMatrix4(mesh.matrixWorld);
+
+    // Normals need to be derived from the final posed triangle in world space.
+    cb.subVectors(vertexC, vertexB);
+    ab.subVectors(vertexA, vertexB);
+    normal.copy(cb.cross(ab).normalize());
+
+    output.push(
+        '\tfacet normal ',
+        normal.x,
+        ' ',
+        normal.y,
+        ' ',
+        normal.z,
+        '\n',
+        '\t\touter loop\n'
+    );
+
+    writeVertex(output, vertexA);
+    writeVertex(output, vertexB);
+    writeVertex(output, vertexC);
+
+    output.push('\t\tendloop\n', '\tendfacet\n');
+}
+
 export default class MinSTLExporter {
 
     parse(scene) {
-        let output = '';
-        let vector = new THREE.Vector3();
-        let normalMatrixWorld = new THREE.Matrix3();
-        let avatarName = window.avatarName.trim().toLowerCase().split(' ').join('-');
+        const avatarName = getAvatarName();
+        const output = ['solid ', avatarName, '\n'];
 
-        // output += 'solid exported\n';
+        scene.updateMatrixWorld(true);
 
         scene.traverse(function (mesh) {
+            if (mesh.isMesh !== true) {
+                return;
+            }
 
-            if (mesh instanceof THREE.Mesh) {
+            const bufferGeometry = mesh.geometry;
+            const bufferPositions = bufferGeometry?.getAttribute?.('position');
 
-                output += 'solid ' + avatarName + '\n';
+            if (!bufferPositions) {
+                return;
+            }
 
-                const {
-                    matrixWorld,
-                    geometry: bufferGeometry,
-                    skeleton
-                } = mesh;
+            const bufferIndices = bufferGeometry.getIndex();
 
-                // var geometry = new THREE.Geometry().fromBufferGeometry( bufferGeometry );
-
-                const bufferIndices = bufferGeometry.getIndex();
-                const bufferSkinIndices = bufferGeometry.getAttribute('skinIndex');
-                const bufferSkinWeights = bufferGeometry.getAttribute('skinWeight');
-                const bufferPositions = bufferGeometry.getAttribute('position');
-
-                function computeFaceNormal(a, b, c) {
-                    const cb = new THREE.Vector3();
-                    const ab = new THREE.Vector3();
-
-                    const vA = new THREE.Vector3().fromBufferAttribute(bufferPositions, a);
-                    const vB = new THREE.Vector3().fromBufferAttribute(bufferPositions, b);
-                    const vC = new THREE.Vector3().fromBufferAttribute(bufferPositions, c);
-
-                    cb.subVectors(vC, vB);
-                    ab.subVectors(vA, vB);
-                    cb.cross(ab);
-
-                    cb.normalize();
-
-                    return cb;
+            if (bufferIndices) {
+                for (let i = 0, len = bufferIndices.count - (bufferIndices.count % 3); i < len; i += 3) {
+                    writeFace(
+                        output,
+                        mesh,
+                        bufferPositions,
+                        bufferIndices.getX(i),
+                        bufferIndices.getX(i + 1),
+                        bufferIndices.getX(i + 2)
+                    );
                 }
 
-                normalMatrixWorld.getNormalMatrix(matrixWorld);
+                return;
+            }
 
-                for (let i = 0, len = bufferIndices.count; i < len; i += 3) {
-                    const a = bufferIndices.getX(i);
-                    const b = bufferIndices.getY(i);
-                    const c = bufferIndices.getZ(i);
-
-                    const faceNormal = computeFaceNormal(a, b, c);
-
-                    vector.copy(faceNormal).applyMatrix3(normalMatrixWorld).normalize();
-                    output += '\tfacet normal ' + vector.x + ' ' + vector.y + ' ' + vector.z + '\n';
-                    output += '\t\touter loop\n';
-
-                    // eslint-disable-next-line no-loop-func
-                    [a, b, c].forEach(vertexIndex => {
-
-                        if (typeof bufferSkinIndices === "undefined") {
-                            vector.fromBufferAttribute(bufferPositions, vertexIndex).applyMatrix4(matrixWorld);
-                            output += '\t\t\tvertex ' + vector.x * 35 + ' ' + vector.y * 35 + ' ' + vector.z * 35 + '\n';
-                        } else {
-                            vector.fromBufferAttribute(bufferPositions, vertexIndex);
-
-                            // see https://github.com/mrdoob/three.js/issues/3187
-                            let boneIndices = [];
-                            boneIndices[0] = bufferSkinIndices.getX(vertexIndex);
-                            boneIndices[1] = bufferSkinIndices.getY(vertexIndex);
-                            boneIndices[2] = bufferSkinIndices.getZ(vertexIndex);
-                            boneIndices[3] = bufferSkinIndices.getW(vertexIndex);
-
-                            let weights = [];
-                            weights[0] = bufferSkinWeights.getX(vertexIndex);
-                            weights[1] = bufferSkinWeights.getY(vertexIndex);
-                            weights[2] = bufferSkinWeights.getZ(vertexIndex);
-                            weights[3] = bufferSkinWeights.getW(vertexIndex);
-
-                            let inverses = [];
-                            inverses[0] = skeleton.boneInverses[boneIndices[0]];
-                            inverses[1] = skeleton.boneInverses[boneIndices[1]];
-                            inverses[2] = skeleton.boneInverses[boneIndices[2]];
-                            inverses[3] = skeleton.boneInverses[boneIndices[3]];
-
-                            let skinMatrices = [];
-                            skinMatrices[0] = skeleton.bones[boneIndices[0]].matrixWorld;
-                            skinMatrices[1] = skeleton.bones[boneIndices[1]].matrixWorld;
-                            skinMatrices[2] = skeleton.bones[boneIndices[2]].matrixWorld;
-                            skinMatrices[3] = skeleton.bones[boneIndices[3]].matrixWorld;
-
-                            let finalVector = new THREE.Vector4();
-                            for (let k = 0; k < 4; k++) {
-                                let tempVector = new THREE.Vector4(vector.x, vector.y, vector.z);
-                                tempVector.multiplyScalar(weights[k]);
-                                //the inverse takes the vector into local bone space
-                                tempVector.applyMatrix4(inverses[k])
-                                    //which is then transformed to the appropriate world space
-                                    .applyMatrix4(skinMatrices[k]);
-                                finalVector.add(tempVector);
-                            }
-                            output += '\t\t\tvertex ' + finalVector.x * 35 + ' ' + finalVector.y * 35 + ' ' + finalVector.z * 35 + '\n';
-                        }
-                    });
-                    output += '\t\tendloop\n';
-                    output += '\tendfacet\n';
-                }
-                output += 'endsolid ' + avatarName + '\n';
+            for (let i = 0, len = bufferPositions.count - (bufferPositions.count % 3); i < len; i += 3) {
+                writeFace(output, mesh, bufferPositions, i, i + 1, i + 2);
             }
         });
-        return output;
+
+        output.push('endsolid ', avatarName, '\n');
+
+        return output.join('');
     }
 }
